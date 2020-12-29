b1916aa274e8ef6217531eb660f5c10d08a18958
Deepti Baral <deeptibaral10@gmail.com>
2020-12-29 13:24:02 -0600

    First 9 questions solutions

---
+++ src/Assignment/Question28.java
@@ -0,0 +1,47 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Question28 {
+
+    public static void main(String[] args) {
+
+        Question28 ob = new Question28();
+        //ob.isPrimeNumber(10);
+        ob.allPrimebetween(1,100);
+
+    }
+
+    public void isPrimeNumber(int num){
+        int temp;
+
+        for(int i = 2; i<=num; i++){
+            temp = num%i;
+            if(temp==0){
+                System.out.println("It is not a prime number");
+                break;
+            }
+            else{
+                System.out.println("It is a prime number");
+                break;
+            }
+        }
+
+    }
+
+    public void allPrimebetween(int start , int  end){
+        int num;
+        for(int i = start; i<=end;i++){
+            int counter = 0;
+
+            for(num = i; num>=1; num-- ){
+                if(i%num ==0){
+                    counter = counter+1;
+                }
+                if(counter==2){
+                    System.out.println("" +i);
+                }
+            }
+        }
+    }
+}
---
+++ src/Assignment/Questions.java
@@ -0,0 +1,38 @@
+package Assignment;
+
+import javax.swing.*;
+
+public class Questions {
+
+
+    public static void main(String[] args) {
+
+        //Questions10a();
+        Questions10c();
+
+
+    }
+
+    public  static void Questions10a(){
+
+        double u  = Double.parseDouble(JOptionPane.showInputDialog("Enter u "));
+        double t = Double.parseDouble(JOptionPane.showInputDialog("Enter t"));
+        double a = Double.parseDouble(JOptionPane.showInputDialog("Enter a"));
+
+        double s = u * t + 0.5 * a * t * t;
+        JOptionPane.showMessageDialog(null, "The S calculated is: " +s);
+
+    }
+
+    public static void Questions10c(){
+
+        double b = Double.parseDouble(JOptionPane.showInputDialog("Enter the value of b"));
+        double a = Double.parseDouble(JOptionPane.showInputDialog("Enter the value of a"));
+        double c = Double.parseDouble(JOptionPane.showInputDialog("Enter the value of c"));
+
+        double x = 0.5 * (-b + Math.sqrt((b * b) - (4 * a * c)));
+        JOptionPane.showMessageDialog(null, "The value of x is : " +x);
+
+
+    }
+}
