de33436bfd61b7ac9c9a3c2fd49f5e88e63fc401
Deepti Baral <deeptibaral10@gmail.com>
2020-12-29 14:03:02 -0600

    Solutions upto 30

--- src/Assignment/Solutions1to9.java
+++ src/Assignment/Solutions1to9.java
@@ -1,2 +1,165 @@
-package Assignment;public class Solutions1to9 {
+package Assignment;
+
+import java.util.Scanner;
+import javax.swing.*;
+
+
+public class Solutions1to9 {
+    public static void main(String[] args) {
+        greetings();
+        introduction();
+        sum();
+        simpleInterest();
+        area();
+        perimeter();
+        cylinderVolume();
+        temperatureCon();
+        weightCon();
+
+    }
+
+    public static void greetings(){
+        System.out.println("Hello, How are you?\n " +
+                "Welcome to the Java Class\n" +
+                "Nice to meet you.\n");
+    }
+
+    public static void introduction(){
+        System.out.println("Hello Everyone, I am Deepti Baral.\n I recently graduated in Computer " +
+                "Information System this December 2020. \n I am looking forward to this class for " +
+                "deep learning and understanding of Java.\n\n");
+    }
+
+    public static void sum(){
+        int firstNumber, secondNumber, s;
+        Scanner number = new Scanner(System.in);
+
+        System.out.println("***Sum of Numbers***");
+        System.out.println("\nEnter the First Number:");
+        firstNumber = number.nextInt();
+        System.out.println("Enter Second Number:");
+        secondNumber = number.nextInt();
+
+        s = firstNumber + secondNumber;
+
+        System.out.println("The Sum of these numbers: " +s);
+    }
+
+    public static void simpleInterest(){
+        float p,t,r,sInterest;
+        Scanner num = new Scanner(System.in);
+
+        System.out.println("\nEnter the Principle Amount:");
+        p = num.nextFloat();
+        System.out.println("Enter the Time:");
+        t = num.nextFloat();
+        System.out.println("Enter the Rate:");
+        r = num.nextFloat();
+
+
+        sInterest= (p*t*r)/100;
+        System.out.println("The Simple Interest is:" +sInterest);
+
+    }
+
+    public static void area(){
+        double r,l,w, b,h,areaCircle, areaRectangle, areaTriangle;
+        Scanner num = new Scanner(System.in);
+
+        System.out.println("\n***For Circle****");
+        System.out.println("Enter the Radius of Circle:");
+        r = num.nextDouble();
+        areaCircle = Math.PI*r*r;
+        System.out.println("The area of Circle is: " +areaCircle);
+
+        System.out.println("\n***For Rectangle****");
+        System.out.println("Enter the length:");
+        l = num.nextDouble();
+        System.out.println("Enter the Width:");
+        w = num.nextDouble();
+        areaRectangle = l * w;
+        System.out.println("The area of the Rectangle is: " +areaRectangle);
+
+        System.out.println("\n***For Triangle****");
+        System.out.println("Enter the Base:");
+        b = num.nextDouble();
+        System.out.println("Enter the Height:");
+        h = num.nextDouble();
+
+
+        areaTriangle = 0.5*b*h;
+        System.out.println("The area of Triangle is:" +areaTriangle );
+    }
+
+    public static void perimeter(){
+        double r, l, w, a,b,c,  periCircle, periRectangle, periTriangle;
+        Scanner num = new Scanner(System.in);
+
+        System.out.println("***For Circle***");
+        System.out.println("Enter the Radius:");
+        r = num.nextDouble();
+        periCircle = 2 * Math.PI * r;
+        System.out.println("The perimeter of Circle is:" +periCircle);
+
+        System.out.println("\n*** For Rectangle***");
+
+        System.out.println("Enter the Length:");
+        l = num.nextDouble();
+        System.out.println("Enter the width:");
+        w = num.nextDouble();
+
+        periRectangle = 2 * (l + w);
+        System.out.println("The perimeter of Rectangle is:"  +periRectangle);
+
+        System.out.println("\n***For Triangle***");
+
+        System.out.println("Enter the First side:");
+        a = num.nextDouble();
+        System.out.println("Enter the second side:");
+        b = num.nextDouble();
+        System.out.println("Enter the third side:");
+        c = num.nextDouble();
+
+        periTriangle = a + b + c;
+        System.out.println("The perimeter of the Triangle is:" +periTriangle);
+    }
+
+    public static void cylinderVolume(){
+        double r, l, volCylinder;
+        Scanner num = new Scanner(System.in);
+        System.out.println("\nEnter the Radius:");
+        r = num.nextDouble();
+
+        System.out.println("Enter the Length:");
+        l = num.nextDouble();
+
+        volCylinder = Math.PI * r * r * l;
+        System.out.println("The volume of the Cylinder is: " +volCylinder);
+    }
+
+    public static void temperatureCon(){
+        double f1,c1, f2,c2;
+        Scanner num = new Scanner(System.in);
+
+        System.out.println("\nEnter the temperature in Fahrenheit");
+        f1 = num.nextDouble();
+        c1 =  ( f1 - 32) * 5/9 ;
+        System.out.println("The equivalent Temperature in Celsius is : " +c1);
+
+        System.out.println("Enter the Celsius Temperature to convert");
+        c2 = num.nextDouble();
+        f2 = 0.2 * (9*c1 + 160);
+        System.out.println("The Equivalent Fahrenheit Temperature is: " +f2);
+    }
+
+    public static void weightCon(){
+        double p, k ;
+        Scanner num = new Scanner(System.in);
+
+        System.out.println("\nEnter Pound to Convert into KG");
+        p = num.nextDouble();
+
+        k = p * 0.454;
+        System.out.println("The Equivalent KG is " +k);
+    }
 }
--- src/Assignment/Solutions20to28.java
+++ src/Assignment/Solutions20to28.java
@@ -1,2 +1,179 @@
-package Assignment;public class Solutions20to28 {
+package Assignment;
+
+import java.util.Scanner;
+
+public class Solutions20to28 {
+    public static void main(String[] args) {
+        //question20();
+        //question21();
+        //question22();
+        //question23();
+        //question24();
+        //question25();
+        //question26();
+        question27();
+        //question28();
+        //question29();
+
+    }
+
+    public static void question20(){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first 2 digit number:");
+        int i = input.nextInt();
+        System.out.println("Enter second 2 digit number:");
+        int j = input.nextInt();
+        System.out.println("Enter your operator to perform");
+        char choice = input.next().charAt(0);
+
+        switch (choice){
+            case '+':
+                int sum = i+j;
+                System.out.println("The sum of number you entered is" +sum);
+                break;
+
+            case '-':
+                int sub;
+                if(i>j){
+                    sub = i-j;
+                    System.out.println("The difference between numbers is " +sub);
+                    break;
+                }
+                else{
+                    sub = j-i;
+                    System.out.println("The difference between numbers is " +sub);
+                    break;
+                }
+            case '/':
+                double div = i/j;
+                System.out.println("The answer of division is" +div);
+                break;
+
+            case '*':
+                int mul = i*j;
+                System.out.println("The answer of the multiplication is " +mul);
+                break;
+        }
+    }
+
+    public static void question21(){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any number from 1 to 7");
+        int choice = input.nextInt();
+
+        switch (choice){
+            case 1 :
+                System.out.println("Sunday");
+                break;
+            case 2 :
+                System.out.println("Monday");
+                break;
+            case 3 :
+                System.out.println("Tuesday");
+                break;
+            case 4 :
+                System.out.println("Wednesday");
+                break;
+            case 5 :
+                System.out.println("Thursday");
+                break;
+            case 6 :
+                System.out.println("Friday");
+                break;
+            case 7 :
+                System.out.println("Saturday");
+                break;
+        }
+    }
+
+    public static void question22(){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any integer:");
+        int n = input.nextInt();
+
+        for(int i=1;i<=10;i++){
+            System.out.println("" +n+ "*" +i+ "=" +n*i);
+        }
+    }
+
+    public static void question23(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any number to find the sum");
+        int n = input.nextInt();
+        int sum=0;
+
+        for(int i=1; i<=n; i++){
+            sum = sum+i;
+        }
+
+        System.out.println("The sum of the number to nth position is: " +sum);
+    }
+    public static void question24(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any number to find factorial:");
+        int n = input.nextInt();
+        int fact = 0;
+
+        for(int i = n ; i>=1 ; i--){
+
+            fact = fact*i;
+        }
+
+        System.out.println("The factorial of the number is " +fact);
+    }
+
+    public static void question25(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of the students");
+        int n = input.nextInt();
+        int highest = 0;
+
+        for(int i = 1; i<=n ; i++){
+            System.out.println("Enter the name of the Student:");
+            String name = input.next();
+            System.out.println("Enter the score of the Student:");
+            int score = input.nextInt();
+
+            if (highest<score){
+                highest = score;
+            }
+        }
+
+        System.out.println("The highest Score is " +highest);
+    }
+
+    public static void question26(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any number to reverse");
+        int n = input.nextInt();
+        int reversenum = 0;
+
+        while (n!=0){
+            reversenum = reversenum * 10;
+            reversenum = reversenum + n%10;
+            n = n/10;
+        }
+
+        System.out.println("Thr reverse of the number is " +reversenum);
+    }
+
+    public static void question27(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any number");
+        int n = input.nextInt();
+        int product = 1;
+        int sum = 0;
+
+        while( n >0){
+            int i = n %10;
+            sum = sum + i;
+            product = product * i;
+            n = n/10;
+        }
+        System.out.println("The sum of digits is " +sum);
+        System.out.println("The product of the digits is " +product);
+    }
 }
