7eda6d434c51d44a051bea9ca7e3df29f49c0951
Deepti Baral <deeptibaral10@gmail.com>
2021-01-25 10:20:19 -0600

    Solution upto 45

--- src/Assignment/Solution41to45.java
+++ src/Assignment/Solution41to45.java
@@ -108,27 +108,21 @@ public class Solution41to45 {
         FileReader file = new FileReader("C:\\Users\\deept\\OneDrive\\Desktop\\Java\\File Reader.txt");
         BufferedReader br = new BufferedReader(file);
 
-        //Reads each line
         while((line = br.readLine()) != null) {
             String string[] = line.toLowerCase().split("([,.\\s]+) ");
-            //Adding all words generated in previous step into words
             for(String s : string){
                 words.add(s);
                 System.out.println("" +word);
             }
         }
 
-        //Determine the most repeated word in a file
         for(int i = 0; i < words.size(); i++){
             count = 1;
-            //Count each word in the file and store it in variable count
             for(int j = i+1; j < words.size(); j++){
                 if(words.get(i).equals(words.get(j))){
                     count++;
                 }
             }
-            //If maxCount is less than count then store value of count in maxCount
-            //and corresponding word to variable word
             if(count >=2){
                 word = words.get(i);
                 System.out.println("The Duplicated Words are: " + word);
