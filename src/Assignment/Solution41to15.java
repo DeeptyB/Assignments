8e4807df5a54b60b7b7700e14b1b923bbaf097de
Deepti Baral <deeptibaral10@gmail.com>
2021-01-25 00:00:44 -0600

    Menu Project Completed

--- src/Assignment/Solution41to15.java
+++ src/Assignment/Solution41to15.java
@@ -1,2 +1,12 @@
-package Assignment;public class Solution41to15 {
+package Assignment;
+
+public class Solution41to15 {
+    public static void main(String[] args) {
+        Solution41to15 obj = new Solution41to15();
+        obj.solution41();
+    }
+
+    public void solution41(){
+
+    }
 }
